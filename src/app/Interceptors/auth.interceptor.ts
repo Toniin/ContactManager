d1979b6159ea66dc59fa7d73d3647fa670803b19
Antonin <antonin.chaudiere@hotmail.fr>
2024-07-31 14:59:37 +0200

    Change class auth interceptor into functional interceptor

--- src/app/Interceptors/auth.interceptor.ts
+++ src/app/Interceptors/auth.interceptor.ts
@@ -1,22 +1,37 @@
 import {Injectable} from "@angular/core";
-import {HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest} from "@angular/common/http";
+import {HttpEvent, HttpHandler, HttpHandlerFn, HttpHeaders, HttpInterceptor, HttpRequest} from "@angular/common/http";
 import {Observable} from "rxjs";
 
-@Injectable()
-export class AuthInterceptor implements HttpInterceptor {
-
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    // Bypass the token in headers on login/register page
-    if (request.url.includes('/auth/login') || request.url.includes('/auth/register')) {
-      return next.handle(request);
-    }
-
-    let token = localStorage.getItem("Token");
-    request = request.clone({
-      setHeaders:
-        {Authorization: `Bearer ${token}`}
-    });
-
-    return next.handle(request);
+// Functional interceptor
+export function AuthInterceptor(request: HttpRequest<any>, next: HttpHandlerFn): Observable<HttpEvent<any>> {
+  if (request.url.includes('/auth/login') || request.url.includes('/auth/register')) {
+    return next(request);
   }
+
+  let token = localStorage.getItem("Token");
+  request = request.clone({
+    setHeaders:
+      {Authorization: `Bearer ${token}`}
+  });
+
+  return next(request);
 }
+
+// @Injectable()
+// export class AuthInterceptor implements HttpInterceptor {
+//
+//   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+//     // Bypass the token in headers on login/register page
+//     if (request.url.includes('/auth/login') || request.url.includes('/auth/register')) {
+//       return next.handle(request);
+//     }
+//
+//     let token = localStorage.getItem("Token");
+//     request = request.clone({
+//       setHeaders:
+//         {Authorization: `Bearer ${token}`}
+//     });
+//
+//     return next.handle(request);
+//   }
+// }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,20 +1,19 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
-import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi} from "@angular/common/http";
+import {routes} from './app.routes';
+import {provideHttpClient, withInterceptors} from "@angular/common/http";
 import {provideAnimations} from "@angular/platform-browser/animations";
 import {AuthInterceptor} from "./Interceptors/auth.interceptor";
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes),
     provideHttpClient(),
     provideAnimations(),
     provideHttpClient(
-      withInterceptorsFromDi()
+      withInterceptors([AuthInterceptor])
     ),
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
   ],
 };
