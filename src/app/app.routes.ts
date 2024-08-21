7df49d5f89de822bd9baac7316b1ab9d4f0663e5
Toniin <antonin.chaudiere@hotmail.fr>
2024-08-21 09:43:40 +0200

    Add lazy loading on routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
 import {AuthGuard} from "./Guards/auth.guard";
-import {LoginPageComponent} from "./Views/public/login-page/login-page.component";
-import {RegisterPageComponent} from "./Views/public/register-page/register-page.component";
-import {ErrorPageComponent} from "./Views/public/error-page/error-page.component";
-import {AddContactPageComponent} from "./Views/protected/add-contact-page/add-contact-page.component";
-import {ContactsPageComponent} from "./Views/protected/contacts-page/contacts-page.component";
+// import {LoginPageComponent} from "./Views/public/login-page/login-page.component";
+// import {RegisterPageComponent} from "./Views/public/register-page/register-page.component";
+// import {ErrorPageComponent} from "./Views/public/error-page/error-page.component";
+// import {AddContactPageComponent} from "./Views/protected/add-contact-page/add-contact-page.component";
+// import {ContactsPageComponent} from "./Views/protected/contacts-page/contacts-page.component";
 
 const appName = "ContactManager"
 
@@ -14,24 +14,29 @@ export const routes: Routes = [
     pathMatch: 'full'
   },
   { path: 'login',
-    component: LoginPageComponent,
+    loadComponent: () => import('./Views/public/login-page/login-page.component').then(mod => mod.LoginPageComponent),
+    // component: LoginPageComponent,
     title: `Login - ${appName}`
   },
   { path: 'register',
-    component: RegisterPageComponent,
+    loadComponent: () => import('./Views/public/register-page/register-page.component').then(mod => mod.RegisterPageComponent),
+    // component: RegisterPageComponent,
     title: `Register - ${appName}`
   },
   { path: "contacts",
-    component: ContactsPageComponent,
+    loadComponent: () => import('./Views/protected/contacts-page/contacts-page.component').then(mod => mod.ContactsPageComponent),
+    // component: ContactsPageComponent,
     canActivate: [AuthGuard],
     title: `Contacts - ${appName}`
   },
   { path: "contacts/add",
-    component: AddContactPageComponent,
+    loadComponent: () => import('./Views/protected/add-contact-page/add-contact-page.component').then(mod => mod.AddContactPageComponent),
+    // component: AddContactPageComponent,
     canActivate: [AuthGuard],
     title: `Add contact - ${appName}`
   },
   { path: "**",
-    component: ErrorPageComponent,
+    loadComponent: () => import('./Views/public/error-page/error-page.component').then(mod => mod.ErrorPageComponent),
+    // component: ErrorPageComponent,
     title: `Not found - ${appName}` }
 ];
